189736bd13d2e42138eb7214c54bbe39fb9b7d5c
Vishesh Singh <VisheshSingh16@gmail.com>
2020-03-29 09:42:23 -0500

    Scroll events with INtersection Observer 👁👁👀

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,19 @@
-console.log('IT WORKS!');
+// DOM Elements
+const terms = document.querySelector('.terms-and-conditions');
+const button = document.querySelector('.accept');
+
+// Observer callback
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    ob.unobserve(terms.lastElementChild);
+  } else {
+    button.disabled = true;
+  }
+}
+
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1
+});
+ob.observe(terms.lastElementChild);
