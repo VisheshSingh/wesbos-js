7237c576f86d4c629ec210b0e54367a2cdedaf3e
Vishesh Singh <VisheshSingh16@gmail.com>
2020-04-06 22:45:27 -0500

    drawing arect over face detect 😵⬛

--- exercises/55 - Face Detection Censorship/pixelated-face.js
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -1,6 +1,8 @@
 const video = document.querySelector('.webcam');
 const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
+ctx.strokeStyle = '#ffc600';
+ctx.lineWidth = 2;
 const faceCanvas = document.querySelector('.face');
 const faceCtx = canvas.getContext('2d');
 
@@ -22,8 +24,14 @@ async function populateVideo() {
 
 async function detect() {
   const faces = await faceDetector.detect(video);
-  console.log(faces.length);
+  // console.log(faces.length);
+  faces.forEach(drawFace);
   requestAnimationFrame(detect);
 }
 
+function drawFace(face) {
+  const { width, height, top, left } = face.boundingBox;
+  console.log({ width, height, top, left });
+  ctx.strokeRect(left, top, width, height);
+}
 populateVideo().then(detect);
